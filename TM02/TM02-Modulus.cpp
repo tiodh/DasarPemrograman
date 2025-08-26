903a7c736f97c5c6906fe7ea41fdd025ae07692f
Tio Dharmawan <tio.pssi@unej.ac.id>
2025-08-26 20:22:18 +0700

    Pertemuan kedua, contoh modulus

---
+++ TM02/TM02-Modulus.cpp
@@ -0,0 +1,38 @@
+// # TM02 - Tipe Data, Variable, dan Operator Aritmatika
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Deklarasi Variabel
+    int nilai, hasil;
+
+    nilai = 1;
+    cout << "Input: " << nilai << endl;
+    hasil = nilai % 2;
+
+    cout << "Proses: " << nilai << "%2 = " << endl;
+    cout << "Hasil: " << hasil << endl << endl;
+    // ==============================================
+    nilai = 2;
+    cout << "Input: " << nilai << endl;
+    hasil = nilai % 2;
+
+    cout << "Proses: " << nilai << "%2 = " << endl;
+    cout << "Hasil: " << hasil << endl << endl;
+    // ==============================================
+    nilai = 3;
+    cout << "Input: " << nilai << endl;
+    hasil = nilai % 2;
+
+    cout << "Proses: " << nilai << "%2 = " << endl;
+    cout << "Hasil: " << hasil << endl << endl;
+    // ==============================================
+    nilai = 4;
+    cout << "Input: " << nilai << endl;
+    hasil = nilai % 2;
+
+    cout << "Proses: " << nilai << "%2 = " << endl;
+    cout << "Hasil: " << hasil << endl << endl;
+}
